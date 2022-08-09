2c2663fad5372bff40f8023a547284ada22d666f
Steven Sparks <steven@gms.dev>
2022-08-09 13:00:14 -0400

    Final

--- invoice_project/settings.py
+++ invoice_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-(3o*0kt%oi*#10nvggli!l-_5c0&i#tes3jrih#5dse^4z@uyj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
